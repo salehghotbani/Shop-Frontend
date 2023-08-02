97acb6b424092ecd22ce169dc27baf38a4140312
salehghotbani <saleh313ghotbani@gmail.com>
2023-08-02 12:09:58 +0330

    Build part of the structure of site

---
+++ src/Components/Header.jsx
@@ -0,0 +1,60 @@
+import {
+  Box,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  Text,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+} from '@chakra-ui/react';
+import shoppingCartLinearIcon from '../assets/icons/Shop/vuesax/linear/shopping-cart.svg';
+import userIcon from '../assets/icons/Users/vuesax/linear/user.svg';
+import menuIcon from '../assets/icons/Essetional/vuesax/linear/menu.svg';
+import loginIcon from '../assets/icons/Arrow/vuesax/linear/login.svg'
+
+export const Header = () => {
+  const UserIcon = () => {
+    return (
+      <Popover>
+        <PopoverTrigger>
+          <Box p={'6px'} backgroundColor={'white'} borderRadius={'100%'} cursor={'pointer'}>
+            <Image src={userIcon} />
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent w={'200px'}>
+          <PopoverBody dir={'rtl'}>
+            <HStack>
+              <Image src={loginIcon}/>
+              <Text cursor={'default'}>ورود</Text>
+            </HStack>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    );
+  };
+
+  return (
+    <>
+      <Box position={'sticky'} top={0} px={7} py={3} backgroundColor={'#1D3347'} className={'box_shadow'}>
+        <Grid templateColumns='repeat(5, 1fr)' gap={4}>
+          <GridItem colStart={0} colEnd={2}>
+            <HStack spacing={3}>
+              <Box p={'6px'} backgroundColor={'white'} borderRadius={'100%'} cursor={'pointer'}>
+                <Image src={shoppingCartLinearIcon} />
+              </Box>
+              <UserIcon />
+            </HStack>
+          </GridItem>
+          <GridItem colStart={6} colEnd={6}>
+            <Box p={'6px'} backgroundColor={'white'} borderRadius={'100%'} cursor={'pointer'}>
+              <Image src={menuIcon} />
+            </Box>
+          </GridItem>
+        </Grid>
+      </Box>
+    </>
+  );
+};
