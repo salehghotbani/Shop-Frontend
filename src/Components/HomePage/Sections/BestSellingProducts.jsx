7aff3963b95e357f44725fd55a3c3a978e87fb5e
mohmmdkhosravi <amirmohkhosravi@gmail.com>
2023-08-04 18:43:25 +0330

    reformatted intellij

--- src/Components/HomePage/Sections/BestSellingProducts.jsx
+++ src/Components/HomePage/Sections/BestSellingProducts.jsx
@@ -82,6 +82,7 @@ export const BestSellingProducts = () => {
                   </motion.div>
                 </Center>
               </Box>
+
             </Center>
           ))}
         </Carousel>;
